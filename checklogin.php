8e9edbdc85e4de8ff4a4ca8aae417d24a60ab5ac
zieglerz <zieglerz@students.zcu.cz>
2016-03-14 16:28:24 +0100

    První upload - základní web
    Hrubý základ home page a připravená login page s funčkním odesíláním z
    formuláře.

---
+++ checklogin.php
@@ -0,0 +1,13 @@
+<?php
+
+    $username;
+    $password;
+
+    $username = trim($_POST['usr']);
+    $password = trim($_POST['pwd']);
+
+
+    echo "Username: ".$username;
+    echo "Password: ".$password;
+
+?>
