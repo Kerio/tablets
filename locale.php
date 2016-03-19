0ef17b969d3a8eff19ed631a99ade2694c201add
zieglerz <zieglerz@students.zcu.cz>
2016-03-19 19:05:44 +0100

    Změna formátu stránky z HTML na PHP + Použití Locales
    Pro možnost změny locales - jazyka stránky - bylo nutné předělat stránky
    z .html na .php

---
+++ locale.php
@@ -0,0 +1,32 @@
+<?php
+
+//CZ
+$phrase['cz']['lang'] = "cz";
+$phrase['cz']['kerio_b'] = "Kerio benefity";
+$phrase['cz']['nav_home'] = "Domů";
+$phrase['cz']['nav_lang'] = "Jazyk";
+$phrase['cz']['nav_lang_cz'] = "Čeština";
+$phrase['cz']['nav_lang_eng'] = "Angličtina";
+$phrase['cz']['login'] = "Přihlášení";
+$phrase['cz']['login_mail'] = "E-mail";
+$phrase['cz']['login_mail_text'] = "Zdadejte e-mail";
+$phrase['cz']['login_pwd'] = "Heslo";
+$phrase['cz']['login_pwd_text'] = "Zadejte heslo";
+$phrase['cz']['submit'] = "Přihlásit";
+$phrase['cz']['footer_text'] = "Webovou aplikaci vytvořila skupina ZMJ";
+
+//ENG
+$phrase['eng']['lang'] = "eng";
+$phrase['eng']['kerio_b'] = "Kerio benefits";
+$phrase['eng']['nav_home'] = "Home";
+$phrase['eng']['nav_lang'] = "Language";
+$phrase['eng']['nav_lang_cz'] = "Czech";
+$phrase['eng']['nav_lang_eng'] = "English";
+$phrase['eng']['login'] = "Log in";
+$phrase['eng']['login_mail'] = "E-mail";
+$phrase['eng']['login_mail_text'] = "Enter e-mail";
+$phrase['eng']['login_pwd'] = "Password";
+$phrase['eng']['login_pwd_text'] = "Enter password";
+$phrase['eng']['submit'] = "Log in";
+$phrase['eng']['footer_text'] = "Web application Made By ZMJ group";
+?>
---
+++ zaklad_stranky.php
@@ -0,0 +1,59 @@
+<?php
+//  import locales for translation website
+require 'locale.php';
+
+//  language setting
+if($_GET == NULL){
+    $locale = 'cz';
+}
+else{
+    $locale = $_GET['locale'];
+}
+
+echo '<!DOCTYPE html>';
+echo '<html lang= "'.$phrase[$locale]['lang'].'">';
+echo '    <head>';
+echo '        <meta charset="utf-8">';
+echo '    <!-- automatic width according device -->';
+echo '        <meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '    <!-- import Bootstrap v.3.3.6 -->';
+echo '        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">';
+echo '        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>';
+echo '        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>';
+echo '    <!-- import ccs style -->';
+echo '        <link rel="stylesheet" href="css/main_style.css">';
+echo '    </head>';
+
+echo '    <body>';
+echo '    <!-- navigation bar -->';
+echo '        <nav class="navbar navbar-inverse">';
+echo '            <div class="container">';
+echo '                <div class="navbar-header">';
+echo '                    <a class="navbar-brand" href="index.php">'.$phrase[$locale]['kerio_b'].'</a>';
+echo '                </div>';
+echo '                <ul class="nav navbar-nav">';
+echo '                    <li><a href="zaklad_stranky.php">default page</a></li>';
+echo '                    <li><a href="#">Page 2</a></li>';
+echo '                    <li><a href="#">Page 3</a></li>';
+echo '                </ul>';
+echo '     <!-- exchange language -->';
+echo '                <ul class="nav navbar-nav navbar-right">';
+echo '                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon glyphicon-globe"></span>   '.$phrase[$locale]['nav_lang'].'<span class="caret"></span></a>';
+echo '                        <ul class="dropdown-menu">';
+echo '                            <li><a href="?locale=cz">'.$phrase[$locale]['nav_lang_cz'].'</a></li>';
+echo '                            <li><a href="?locale=eng">'.$phrase[$locale]['nav_lang_eng'].'</a></li>';
+echo '                        </ul>';
+echo '                    </li>';
+echo '                </ul>';
+echo '            </div>';
+echo '        </nav>';
+
+echo '<!-- center of page -->';
+echo '';
+
+echo '    <!-- footer of page -->';
+echo '    <footer class="navbar-fixed-bottom bg-1 text-center">';
+echo '        <p>'.$phrase[$locale]['footer_text'].'</p>';
+echo '    </footer>';
+echo '</body>';
+
